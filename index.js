ef2930a7c6fa8b5d853a57365292059adbfe571b
Bayu <bayuns@icloud.com>
2017-10-29 15:29:27 +0700

    commit fixed

---
+++ index.js
@@ -0,0 +1,40 @@
+Number
+const n = 10;
+let y = 11;
+
+String
+const apos_string = 'hello';
+const quot_string = "hello";
+
+const kalimat = "lalu katanya, \"Hai!\"";
+
+kalimat
+
+const b = true;
+
+r = [1,2,3,4];
+r
+
+rs = Array.from(apos_string);
+rs
+console.log(rs.length);
+
+const o = {
+    1: 1,
+    nama: 'Bayu',
+    kelas: 'Web',
+    partner: {
+        nama: 'Rio',
+        kelas: 'Web',
+    },
+}
+
+console.log(o)
+
+console.log(o.partner.nama)
+
+function add(x,y){
+    return x+y
+}
+
+console.log(add(10,10))
