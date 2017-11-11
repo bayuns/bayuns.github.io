2729384c5e5de0db759df7f59d8e1d771fa786cf
Bayu <bayuns@icloud.com>
2017-11-12 06:18:03 +0700

    commit to project mini progress

---
+++ detail.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KUKALI</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/typicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/Brands.css">
+    <link rel="stylesheet" href="assets/css/Features-Blue.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Product-List.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/System-Status.css">
+</head>
+
+<body>
+    <div>
+        <div class="header-blue">
+            <nav class="navbar navbar-default navigation-clean-search">
+                <div class="container">
+                    <div class="navbar-header"><a class="navbar-brand" href="#">KUKALI for Kota Bandung </a>
+                        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active" role="presentation"><a href="#">Home </a></li>
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">About <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li role="presentation"><a href="#">First Item</a></li>
+                                    <li role="presentation"><a href="#">Second Item</a></li>
+                                    <li role="presentation"><a href="#">Third Item</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="navbar-form navbar-left" target="_self">
+                            <div class="form-group">
+                                <label class="control-label" for="search-field"><i class="glyphicon glyphicon-search"></i></label>
+                                <input class="form-control search-field" type="search" name="search" id="search-field">
+                            </div>
+                        </form>
+                        <p class="navbar-text navbar-right"><a class="navbar-link login" href="#">Log In</a> <a class="btn btn-default action-button" role="button" href="#">Sign Up</a></p>
+                    </div>
+                </div>
+            </nav>
+            <div class="container hero" style="padding:0px;">
+                <ol class="breadcrumb">
+                    <li><a><span>Home</span></a></li>
+                    <li><a><span>Kuliner </span></a></li>
+                    <li><a><span>Nasi Goreng Mafia</span></a></li>
+                </ol>
+                <h1 style="margin:0px;"><strong>Nasi Goreng Mafia</strong></h1>
+                <p style="margin:23px;">Siapkan diri anda ketika akan mengunjungi Tempat Wisata Kuliner di Bandung ini !! Karena apa coba ? Karena anda dan pengunjung lainnya akan duduk makan menikmati menu kuliner nasi goreng satu meja dengan para kelompok mafia dan Preman
+                    seperti Yakuza, GodFather, Triad, Gangster sampai Preman semua kumpul di satu tempat wisata kuliner Nasi Goreng Mafia Bandung. </p>
+            </div>
+            <div class="bg-gray padding-50">
+                <div class="container" id="system-status">
+                    <h1 class="text-center">Sekilas Info</h1>
+                    <p class="text-center">Last update Senin, 11 November 2017 20:00 WIB</p>
+                    <div class="system-status">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2>Overview </h2>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td>Nama </td>
+                                                <td><i class="glyphicon glyphicon-user"></i>Nasi Goreng Mafia</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Hari Buka</td>
+                                                <td><i class="glyphicon glyphicon-ok"></i>Senin - Minggu </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Jam Buka</td>
+                                                <td><i class="glyphicon glyphicon-ok"></i>10:00 - 22:00 WIB</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Rate Harga</td>
+                                                <td><i class="glyphicon glyphicon-ok"></i>Rp20.000-Rp50.000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Status </td>
+                                                <td><i class="glyphicon glyphicon-ok"></i>Halal</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Total Menu</td>
+                                                <td><i class="glyphicon glyphicon-ok"></i>9 Menu</td>
+                                            </tr>
+                                            <tr></tr>
+                                            <tr></tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Alamat <small>(2) </small></h2>
+                                <div class="well">
+                                    <h3><i class="typcn typcn-shopping-cart"></i>Dipatiukur </h3>
+                                    <p class="text-justify">Jalan Dipatiukur No.51, Lebakgede, Coblong, Lebakgede, Coblong, Kota Bandung, Jawa Barat 40132 </p>
+                                    <div class="latest-update-timestamp"><span><strong>*</strong> Harga antar cabang mungkin terdapat selisih harga.</span></div>
+                                </div>
+                                <div class="well">
+                                    <h3><i class="typcn typcn-shopping-cart"></i>Dipatiukur </h3>
+                                    <p class="text-justify">Jalan Dipatiukur No.51, Lebakgede, Coblong, Lebakgede, Coblong, Kota Bandung, Jawa Barat 40132 </p>
+                                    <div class="latest-update-timestamp"><span><strong>*</strong> Harga antar cabang mungkin terdapat selisih harga. </span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="simple-slider">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" style="background-image:url(&quot;assets/img/banner-min.jpg&quot;);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row product-list">
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/godfather-min.png"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <h2><a href="#">GOD FATHER</a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-justify product-description">Rempah special dan menggunakan kecap yang berkualitas, sehingga rasanya makin gurih!</p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp25.500,- </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/gengster-min.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <h2><a href="#">GANGSTER </a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-justify product-description">Nasi goreng dengan racikan rempah rahasia. Ditaburi dengan potongan daun bawang, bikin jadi makin gurih Boss </p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp29.000,- </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/yakuza-min.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <h2><a href="#">YAKUZA </a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="product-description">Dengan campuran BUMBU REMPAH dan KIKIL RENYAH yang menambah selera makan penikmatnya. </p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp29.000,- </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row product-list">
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/brandal-min.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <h2><a href="#">BRANDAL </a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-justify product-description">Nasi goreng dengan campuran kemangi dan rempah rahasia.</p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp20.000 </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/triad-min.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8" style="width:272px;">
+                            <h2><a href="#" style="font-size:16px;">TRIAD </a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-justify product-description">Nasi goreng dengan bumbu rempah spesial dan daging kambing yang lezat.</p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp21.000 </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 product-item">
+                <div class="product-container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="product-image"><img src="assets/img/badboyz-min.jpg"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8" style="width:274px;">
+                            <h2 style="font-size:16px;"><a href="#">BADBOYZ </a></h2></div>
+                    </div>
+                    <div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i><a href="#" class="small-text">82 reviews</a></div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-justify product-description">Kalau makan, air mata bisa keluar saking pedasnya, keingat mantan.</p>
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <button class="btn btn-default" type="button">Review </button>
+                                </div>
+                                <div class="col-xs-6">
+                                    <p class="product-price">Rp22.500 </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/Simple-Slider.js"></script>
+</body>
+
+</html>
